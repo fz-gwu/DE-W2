dd1fa3f589657172f6880055b5652046b86e6f20
fz-gwu <166880392+fz-gwu@users.noreply.github.com>
2024-06-23 23:43:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 litgpt finetune_lora checkpoints/ft/meta-8b \
     --data JSON \
-    --data.json_path dataset/test.json \
+    --data.json_path /home/ec2-user/dataset \
     --out_dir dataset/mydata-finetuned
